aba1300222a76ba959597ff985e17dc80068ed21
eva1999 <evafan@hotaimotor.com.tw>
2023-11-16 01:35:37 +0800

    update all

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from firebase_utils import initialize_firebase, read_firebase_data
 
 app = FastAPI()
 
@@ -10,6 +11,10 @@ templates = Jinja2Templates(directory="templates")
 
 # 靜態文件目錄
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# firebase
+# 初始化 Firebase
+initialize_firebase()
 
 
 # 首頁
@@ -22,3 +27,10 @@ def read_root(request: Request):
 @app.get("/form/{day}")
 def read_form(request: Request, day: str):
     return templates.TemplateResponse("form.html", {"request": request, "day": day})
+
+# 資料庫欄位名稱
+@app.get("/firebase_data")
+def get_firebase_data():
+    # 讀取資料
+    firebase_data = read_firebase_data()
+    return {"firebase_data": firebase_data}
